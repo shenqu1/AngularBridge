c05c0b193d2417b706f2e3f325895b8d6081e8ec
Shen Qu <qushen024@hotmail.com>
2020-07-07 21:18:15 -0400

    Map Component and Leaflet

--- src/app/bridge-info-map/bridge-info-map.component.ts
+++ src/app/bridge-info-map/bridge-info-map.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit,OnChanges, SimpleChanges } from '@angular/core';
+import { Bridge } from '../bridge';
+import {LeafletMap} from './leaflet-map';
 
 @Component({
   selector: 'app-bridge-info-map',
   templateUrl: './bridge-info-map.component.html',
   styleUrls: ['./bridge-info-map.component.css']
 })
-export class BridgeInfoMapComponent implements OnInit {
+export class BridgeInfoMapComponent implements OnInit, AfterViewInit, OnChanges {
 
-  constructor() { }
+  @Input() bridge: Bridge;
+  map: LeafletMap;
+
+  constructor() {
+
+  }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.map = new LeafletMap('map');
+  }
+
+  ngOnChanges(changes: SimpleChanges):void {
+    if (!this.map) {
+      return;
+    }
+
+    if (changes.bridge.currentValue) {
+      const {lat, lng, name} = changes.bridge.currentValue;
+      this.map.update(lat, lng, name);
+    }
   }
 
 }
