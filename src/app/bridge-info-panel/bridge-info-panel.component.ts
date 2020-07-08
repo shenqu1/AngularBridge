42d5552432ff46604f350766d4ac3f9f96d178e3
Shen Qu <qushen024@hotmail.com>
2020-07-07 20:16:32 -0400

    Bridge Info Components

--- src/app/bridge-info-panel/bridge-info-panel.component.ts
+++ src/app/bridge-info-panel/bridge-info-panel.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Bridge } from '../bridge';
+
+function prettyPrintDimension(value: number | null): string {
+  return value ? `${value} m` : 'unknown';
+}
 
 @Component({
   selector: 'app-bridge-info-panel',
@@ -7,9 +12,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BridgeInfoPanelComponent implements OnInit {
 
+  @Input() bridge:Bridge;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  width(): string {
+    return prettyPrintDimension(this.bridge.width);
+  }
+
+  length(): string {
+    return prettyPrintDimension(this.bridge.length);
+  }
+
+  age(): number {
+    const currentYear = (new Date()).getFullYear();
+    return currentYear - this.bridge.year;
   }
 
 }
